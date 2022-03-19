eb1bae1593af1fb3ee453b7a3c2d282716aa3804
Sifflion <dami.m.gonza@gmail.com>
2022-03-19 20:48:47 -0300

    first commit

--- src/Config.js
+++
@@ -1,7 +0,0 @@
-import {config} from 'dotenv';
-config();
-
-//Variable to get into the DB
-export default {
-    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://D-M-Gonzalez:KA82lk-27202@webshopexampledb.dibqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-}

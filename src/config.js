f41ced339632e83bc02d9aedc8e050c2ae10f107
Damian Martin Gonzalez <dami.m.gonza@gmail.com>
2022-03-19 21:19:01 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ config();
 
 //Variable to get into the DB
 export default {
-    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://D-M-Gonzalez:KA82lk-27202@webshopexampledb.dibqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://D-M-Gonzalez:KA82lk-27202@webshopexampledb.dibqv.mongodb.net/WebShopExample?retryWrites=true&w=majority',
 }
